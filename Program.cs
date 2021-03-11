12ac09f31036a57dfa1d921da366c3e7d49a93b0
Hasan Chamdany <hasanchamdany@gmail.com>
2021-03-11 21:18:37 +0700

    Project Aplikasi Profider Wifi

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection.Metadata;
+using System;
+namespace WifiProfiderApk{
+    public class Program{
+
+        public string username;
+        public string Password;
+
+        static void Main(string[] args){
+            Console.WriteLine("---Selamat Datang di Wifi-Profider Apk---");
+            Console.WriteLine("Apakah Anda memiliki Akun ? (yes/no)");
+            string pilih1 = Console.ReadLine();
+            if(pilih1 == "yes"){
+                 Console.WriteLine("Enter Username:");
+                 username = Console.ReadLine();
+                 Console.WriteLine("Enter Password:");
+                 Password = Console.ReadLine();
+                 // Lanjut ke Tagihan (payment)
+            }else if(pilih1 == "no"){
+                 Console.WriteLine("Ingin Mendaftar? (yes/no)");
+                 string pilih2 = Console.ReadLine();
+                 if(pilih2 == "yes"){
+                     //Register
+                     User user = new User();
+                     user.AccountRegister();
+                     Console.WriteLine("Apakah Ingin Berlangganan ? (yes/no)");
+                     string pilih3 = Console.ReadLine();
+                     if(pilih3 == "yes"){
+                        user.WifiRegister();
+                     }
+                 }
+            }else {
+                Console.WriteLine("Error, Tidak ada Inputan Selain yes/no");
+            }
+           
+
+        }
+    }
+}
---
+++ user.cs
@@ -0,0 +1,53 @@
+using System;
+namespace WifiProfiderApk{
+    public class User{
+            string NIK;
+            string alamat;
+            string IP;
+            string nama;
+            string Username;
+            string Password;
+
+        public void AccountRegister(){
+            Console.WriteLine("Masukkan Nama Lengkap: ");
+            nama = Console.ReadLine();
+            Console.WriteLine("Masukkan NIK: ");
+            NIK = Console.ReadLine();
+            Console.WriteLine("Set Username Anda = ");
+            Username = Console.ReadLine();
+            Console.WriteLine("Set Password Anda = ");
+            Password = Console.ReadLine();
+        }
+        public void WifiRegister(){
+            Console.WriteLine("Pilih Jenis Paket: (1/2/3)");
+            Console.WriteLine("1. Paket 100 Mb/s");
+            Console.WriteLine("2. Paket 50 Mb/s");
+            Console.WriteLine("3. Paket 10 Mb/s");
+            int pilih4 = Convert.ToInt32(Console.ReadLine());
+            if(pilih4 == 1){
+                Console.WriteLine("Masukkan Alamat Pemasangan: ");
+                alamat = Console.ReadLine();
+                Console.WriteLine("Set IP Anda: ");
+                IP = Console.ReadLine();
+                Console.WriteLine("Paket Wifi 100 Mb/s Akan di pasangkan dalam 24 jam di alamat anda.");
+                Console.WriteLine("----Terima Kasih----");
+            }else if(pilih4 == 2){
+                Console.WriteLine("Masukkan Alamat Pemasangan: ");
+                alamat = Console.ReadLine();
+                Console.WriteLine("Set IP Anda: ");
+                IP = Console.ReadLine();
+                Console.WriteLine("Paket Wifi 50 Mb/s Akan di pasangkan dalam 24 jam di alamat anda.");
+                Console.WriteLine("----Terima Kasih----");
+            }else if(pilih4 == 3){
+                Console.WriteLine("Masukkan Alamat Pemasangan: ");
+                alamat = Console.ReadLine();
+                Console.WriteLine("Set IP Anda: ");
+                IP = Console.ReadLine();
+                Console.WriteLine("Paket Wifi 10 Mb/s Akan di pasangkan dalam 24 jam di alamat anda.");
+                Console.WriteLine("----Terima Kasih----");
+            }else{
+                Console.WriteLine("Error, Out of Option");
+            }
+        }
+    }
+}
